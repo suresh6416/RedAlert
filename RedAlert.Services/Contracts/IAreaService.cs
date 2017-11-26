ef855b9a880cf1978596cb4a2d02d43d558a2f69
Suresh <suresh.rongali.mca@gmail.com>
2017-11-26 19:04:47 +0530

    Implemented area

---
+++ RedAlert.Services/Contracts/IAreaService.cs
@@ -0,0 +1,16 @@
+﻿using RedAlert.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedAlert.Services.Contracts
+{
+    public interface IAreaService
+    {
+        List<Area> Get();
+        Area GetById(int id);
+        void Save(Area data, string loggedInUserName);
+    }
+}
---
+++ RedAlert.Services/Services/AreaService.cs
@@ -0,0 +1,47 @@
+﻿using RedAlert.Entities.Context;
+using RedAlert.Entities.Models;
+using RedAlert.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedAlert.Services.Services
+{
+    public class AreaService : IAreaService
+    {
+        APIContext _webcontext = new APIContext();
+
+        public List<Area> Get()
+        {
+            return _webcontext.Areas.Where(m => m.IsActive == true).ToList();
+        }
+
+        public Area GetById(int id)
+        {
+            return _webcontext.Areas.Where(m => m.ID == id && m.IsActive == true).FirstOrDefault();
+        }
+
+        public void Save(Area data, string loggedInUserName)
+        {
+            if (data.ID == 0)
+            {
+                data.IsActive = true;
+                data.CreatedBy = loggedInUserName;
+                data.CreatedOn = DateTime.Now;
+                _webcontext.Areas.Add(data);
+            }
+            else
+            {
+                var module = _webcontext.Areas.Where(m => m.ID == data.ID).FirstOrDefault();
+                module.Name = data.Name;
+                module.Description = data.Description;
+                module.UpdatedBy = loggedInUserName;
+                module.UpdatedOn = DateTime.Now;
+            }
+
+            _webcontext.SaveChanges();
+        }
+    }
+}
