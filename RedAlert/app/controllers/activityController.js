9887a73ba67f377fada930a36bcf8434057f82ad
Suresh <suresh.rongali.mca@gmail.com>
2017-11-29 00:30:55 +0530

    Added activity service

--- RedAlert/app/controllers/activityController.js
+++ RedAlert/app/controllers/activityController.js
@@ -1,5 +1,43 @@
-﻿angular.module('RedAlertApp').controller('activityController', ['$rootScope', '$scope', '$state', function ($rootScope, $scope, $state) {
+﻿angular.module('RedAlertApp').controller('activityController', ['$rootScope', '$scope', '$state', 'activityService', function ($rootScope, $scope, $state, activityService) {
     $scope.$on('$viewContentLoaded', function () {
-
+        $scope.Activities = [];
+        $scope.Activity = "";
+        $scope.CurrentActivityId = 0;
     });
+
+    $scope.get = function () {
+        activityService.get().then(function (response) {
+            $scope.Activities = response.Data;
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+    $scope.save = function () {
+        if ($scope.frmActivity.$valid) {
+            activityService.save($scope.Activity).then(function (response) {
+                $scope.get();
+                toaster.success({ title: "Success", body: "Activity saved successfully" });
+            }, function (err) {
+                console.log(err);
+            });
+        }
+    };
+
+    $scope.getById = function (activity) {
+        activityService.getById(activity.ID).then(function (response) {
+            $scope.Activity = response.Data;
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+    $scope.getCurrentRecordId = function () {
+        activityService.getCurrentRecordId().then(function (response) {
+            $scope.CurrentActivityId = response.Data;
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
 }]);
