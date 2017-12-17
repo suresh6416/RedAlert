1e98d93fcdc05f1aa9a77812de16eaa001b000ae
lenovo <lenovo@DESKTOP-91RPKAL>
2017-12-17 10:25:35 +0530

    added grid in compliance page

--- RedAlert/app/controllers/complianceController.js
+++ RedAlert/app/controllers/complianceController.js
@@ -1,6 +1,90 @@
-﻿angular.module('RedAlertApp').controller('complianceController', ['$rootScope', '$scope', '$state', function ($rootScope, $scope, $state) {
+﻿angular.module('RedAlertApp').controller('complianceController', ['$rootScope', '$scope', '$state', 'complianceService', 'toaster', 'NgTableParams','$filter', function ($rootScope, $scope, $state, complianceService, toaster, NgTableParams,$filter) {
     $scope.$on('$viewContentLoaded', function () {
-
+        $scope.Compliances = [];
+        $scope.Compliance = new ComplianceModel();
+        $scope.getCurrentRecordId();
+        $scope.isShowComplianceGrid = false;
+        $scope.isLoading = false;
+        $scope.StatusList = ["Open", "Complete", "Cancel"];
+        $scope.DeviationAcceptance = [{ value: "true", text: "Yes" }, { value: "false", text: "No" }];
     });
+
+    $scope.get = function () {
+        complianceService.get().then(function (response) {
+            $scope.Compliances = response.Data;
+            $scope.tableParams = new NgTableParams({ page: 1, count: 10 }, {
+                dataset: $scope.Compliances
+            });
+            
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+     $scope.save = function () {
+        $scope.isLoading = true;
+        if ($scope.frmActivityInfo.$valid) {
+            $scope.Compliance.AreaId = $scope.SelectedArea.ID;
+            $scope.Compliance.ActivityId = $scope.SelectedActivity.ID;
+
+            complianceService.save($scope.Compliance).then(function (response) {
+                $scope.isLoading = false
+                $scope.Compliance = new ComplianceModel();
+                $scope.get();
+                toaster.success({ title: "Success", body: "Compliance details saved successfully" });
+            }, function (err) {
+                $scope.isLoading = false
+                toaster.error({ title: "Error", body: "Insertion Failed" });
+            });
+        }
+    };
+
+    $scope.getById = function (compliance) {
+        complianceService.getById(compliance.ID).then(function (response) {
+            $scope.Compliance = response.Data;
+            $scope.Compliance.DueDate = $filter('date')($scope.Compliance.DueDate, "yyyy-MM-dd");
+            $scope.Compliance.CompletionDate = $filter('date')($scope.Compliance.CompletionDate, "yyyy-MM-dd");
+            $scope.selectedRow = compliance.ID;
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+    $scope.clear = function () {
+        $scope.Compliance = new ComplianceModel();
+        $scope.getCurrentRecordId();
+    };
+
+    $scope.getCurrentRecordId = function () {
+        complianceService.getCurrentRecordId().then(function (response) {
+            $scope.Compliance.ID = response.Data;
+           
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+    function ComplianceModel() {
+        this.ID = 0;
+        this.AreaId = 0;
+        this.AreaName = "";
+        this.ActivityId = 0;
+        this.ActivityName = "";
+        this.Description = "";
+        this.DueDate = "";
+        this.CompletionDate = "";
+        this.IsDelayed = false;
+        this.ReasonForDelay = "";
+        this.IsDelayAcceptable = false;
+        this.Remarks = "";
+        this.HasDeviation = false;
+        this.DeviationDesc = "";
+        this.IsDeviationAcceptable = "";
+        this.Status = "";
+        this.PreRespPerson = "";
+        this.SecRespPerson = "";
+
+
+    }
 }]);
 
---
+++ RedAlert/app/services/complianceService.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+    angular.module('RedAlertApp').factory('complianceService', ['$http', '$q', '$rootScope', 'redAlertConstants', function ($http, $q, $rootScope, redAlertConstants) {
+
+        var serviceBase = redAlertConstants.apiServiceBaseUri;
+
+        //Get
+        var _get = function () {
+            var deferred = $q.defer();
+
+            $http.get(serviceBase + '/api/compliance').success(function (response) {
+                deferred.resolve(response);
+            }).error(function (err, status) {
+                deferred.reject(err);
+            });
+
+            return deferred.promise;
+        };
+
+        //Save
+        var _save = function (data) {
+            var deferred = $q.defer();
+
+            $http.post(serviceBase + '/api/compliance', data).success(function (response) {
+                deferred.resolve(response);
+            }).error(function (err, status) {
+                deferred.reject(err);
+            });
+
+            return deferred.promise;
+        };        
+
+        //Get Activity By ID
+        var _getById = function (id) {
+            var deferred = $q.defer();
+
+            $http.get(serviceBase + '/api/compliance/GetById/' + id).success(function (response) {
+                deferred.resolve(response);
+            }).error(function (err, status) {
+                deferred.reject(err);
+            });
+
+            return deferred.promise;
+        };
+
+        //Get Current Record Id
+        var _getCurrentRecordId = function () {
+            var deferred = $q.defer();
+
+            $http.get(serviceBase + '/api/compliance/GetCurrentRecordId').success(function (response) {
+                deferred .resolve(response);
+            }).error(function (err, status) {
+                deferred.reject(err);
+            });
+
+            return deferred.promise;
+        };
+
+        return {
+            get: _get,
+            save: _save,          
+            getById: _getById,
+            getCurrentRecordId: _getCurrentRecordId
+        };
+    
+    }]);
+})();
